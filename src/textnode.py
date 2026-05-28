dc7c03851a2b76c009b6dae7c98011f96b1846fb
rmcintyre369 <r.mcintyre369@gmail.com>
2026-05-28 17:54:00 +0100

    refactoring

--- src/textnode.py
+++ src/textnode.py
@@ -29,24 +29,24 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    if text_node.text_type not in TextType:
-        raise Exception("textype invalid")
+
     tag = text_node.text_type
     if tag == TextType.TEXT:
         return LeafNode(tag=None, value=text_node.text)
-    elif tag == TextType.BOLD:
+    if tag == TextType.BOLD:
         return LeafNode(tag="b", value=text_node.text)
-    elif tag == TextType.ITALIC:
+    if tag == TextType.ITALIC:
         return LeafNode(tag="i", value=text_node.text)
-    elif tag == TextType.CODE:
+    if tag == TextType.CODE:
         return LeafNode(tag="code", value=text_node.text)
-    elif tag == TextType.LINK:
+    if tag == TextType.LINK:
         if text_node.url is None:
             raise ValueError("link TextNode must have a url")
         return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
-    else:
+    if tag == TextType.IMAGE:
         if text_node.url is None:
             raise ValueError("img TextNode must have a url")
         return LeafNode(
             tag="img", value="", props={"src": text_node.url, "alt": text_node.text}
         )
+    raise ValueError(f"ivalid text type {text_node.text_type}")
